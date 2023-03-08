99351e2c6435836b372f3cf15b7056e4dfacbab9
TpainMcain <trevorwp77@gmail.com>
2023-03-08 13:27:40 -0600

    add function for timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,41 @@
-// Declare variables for html elements
-// Start screen 
+// Declare variables for html elements. Start screen:
+var viewHSBtn = document.querySelector("#viewHS");
+var timerEl = document.querySelector("#timer");
 var startScreenEl = document.querySelector("#startScreen");
-var startQuizBtn = document.querySelector("#startTest");
-// Quiz questions and answers screen
+var startTestBtn = document.querySelector("#startTest");
+// Quiz questions and answers screen:
 var testEl = document.querySelector("#test");
-var questionEl = document.querySelector("#question");
+var questionsEl = document.querySelector("#questions");
 var answersEl = document.querySelector("#answers");
-// Submitting scores screen
-var inputScoreEl = document.querySelector("#inputScore");
+// Submitting scores screen:
+var submitScoreEl = document.querySelector("#submitScores");
+var userScoreEl = document.querySelector("#score");
 var initialsEl = document.querySelector("#initials");
 var submitInitialsBtnEl = document.querySelector("#submitInitials");
-var userScoreEl = document.querySelector("#score");
-// Highscores screen
+// Highscores screen:
 var highScoresEl = document.querySelector("#highScores");
 var scoresEl = document.querySelector("#scores");
-var goBackBtnEl = document.querySelector("#goBack");
-var clearScoresBtnEl = document.querySelector("#clearScores");
-// Universal variables
-var viewHScoresBtnEl = document.querySelector("#viewHScores");
-var timerEl = document.querySelector("#timer");
+var goBackBtn = document.querySelector("#goBack");
+var resetScoresBtn = document.querySelector("#resetScores");
+// Universal variables:
 var score = 0;
 var currentQ = 0;
 var highScores = [];
 var interval;
 var timeGiven = 75;
 var secondsElapsed = 0;
+// Create function for starting and updating timer.
+function startTimer() {
+    timerEl.textContent = timeGiven;
+    interval = setInterval(function () {
+        secondsElapsed++;
+        timerEl.textContent = timeGiven - secondsElapsed;
+        if (secondsElapsed >= timeGiven) {
+            currentQ = questions.length;
+            nextQuestion();
+        }
+    }, 1000);
+} // Create function for stopping the timer.
+function stopTimer() {
+    clearInterval(interval);
+}
